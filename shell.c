635e48af542bbfa860adf10cb7672b0d4c023c7d
PaolaAndreaGA <paga020964gmail.com>
2021-08-25 16:04:23 -0700

    fixed shell.h

--- shell.c
+++ shell.c
@@ -18,23 +18,27 @@ int main(int argument_count, char **argument_vector, char **environment)
 	{
 		prompt();
 		line_input = _getline();
-		argument_array = tokenization(line_input);
-		route = checker(argument_array[0], argument_array);
-		if (route == 2)
+		if (_strlen(line_input) > 1)
 		{
-			file_name = argument_array[0];
-			argument_array[0] = find_path(argument_array[0], tmp_size, error);
+			argument_array = tokenization(line_input);
+			route = checker(argument_array[0], argument_array);
+			if (route == 2)
+			{
+				file_name = argument_array[0];
+				argument_array[0] = find_path(argument_array[0], tmp_size, error);
 			if (argument_array[0] == error)
 			{
 				argument_array[0] = get_cwd(file_name, error);
 				if (argument_array[0] == file_name)
 					write(1, error, 5);
 			}
+			}
+			if (argument_array[0] != error)
+				status = execute(argument_array, line_input, environment, route);
+			free(argument_array);
 		}
-		if (argument_array[0] != error)
-			status = execute(argument_array, line_input, environment, route);
 		free(line_input);
-		free(argument_array);
+
 	}
 	if (!argument_count)
 		(void)argument_count;
